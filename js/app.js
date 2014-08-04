eb2b86e21fc050bd1d8a899c11da3406141bed4a
Pieter van Leeuwen <p.vanleeuwen@noterik.nl>
2014-08-04 11:36:35 +0200

    Fix statistics

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@
 	
 	// get github stat
 	var getGithubStats = function(){
-		var url="proxy.php";
+		var url="http://os.noterik.com/github_stats.json";
 		$.getJSON(url,function(json){
         	$('#public_repos').html(json.public_repos);
         	$('#lines_of_code_current_week').html(json.lines_of_code_current_week);
--- proxy.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$jsonurl = "http://os.noterik.com/github_stats.json";
-	$json = file_get_contents($jsonurl);
-	echo $json;
-?>
